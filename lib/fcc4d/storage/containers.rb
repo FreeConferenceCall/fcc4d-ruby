769ffd7bc25ed0ca30b82fdc64afdbc96fdd8537
Leonid Krinitsyn <leonid.krinitsyn@freeconferencecall.com>
2017-10-11 17:03:18 +0300

    Patch storage container

--- lib/fcc4d/storage/containers.rb
+++ lib/fcc4d/storage/containers.rb
@@ -20,6 +20,15 @@ module FCC4D
           def find sid
             client.post @content_type, api_path(sid)
           end
+
+          def patch sid, options
+            params = {}
+            params[:integer_key_1] = options[:integer_key_1] if options[:integer_key_1]
+            params[:integer_key_2] = options[:integer_key_2] if options[:integer_key_2]
+            params[:string_key_1] = options[:string_key_1] if options[:string_key_1]
+            params[:string_key_2] = options[:string_key_2] if options[:string_key_2]
+            client.patch @content_type, api_path(sid), params
+          end
         end
       end
     end
