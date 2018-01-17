12e91026f30f547330f920dd71fb073178e1da13
Leonid Krinitsyn <leonid.krinitsyn@freeconferencecall.com>
2018-01-17 16:53:11 +0300

    add partner attributes field

--- lib/fcc4d/partners.rb
+++ lib/fcc4d/partners.rb
@@ -20,7 +20,8 @@ module FCC4D
             billing_email: options[:billing_email],
             tech_email: options[:tech_email],
             company_name: options[:company_name],
-            phonenumber: options[:phonenumber]
+            phonenumber: options[:phonenumber],
+            attributes: options[:attributes]
           }
 
           client.post @content_type, api_path, params
