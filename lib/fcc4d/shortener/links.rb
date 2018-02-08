4441fadd0f891d17e7b8c376100d64e0b1084669
Leonid Krinitsyn <leonid.krinitsyn@freeconferencecall.com>
2018-02-08 15:33:26 +0300

    Moved links shortener reue param to query

--- lib/fcc4d/shortener/links.rb
+++ lib/fcc4d/shortener/links.rb
@@ -8,9 +8,10 @@ module FCC4D
               destination_url: options[:destination_url],
               domain_sid: options[:domain_sid],
               maximum_ttl: options[:maximum_ttl] || "-1",
-              reuse: options[:reuse] == false ? false : true
             }
-            client.post @content_type, api_path, params
+
+            reuse = options[:reuse] == false ? false : true
+            client.post @content_type, api_path(nil, {reuse: reuse}), params
           end
 
         end
