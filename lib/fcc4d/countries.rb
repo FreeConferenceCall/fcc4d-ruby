6c92a2180f6179779f2412d5ec5d2765432be2e3
Leonid Krintsyn <leonid.krinitsyn@freeconferencecall.com>
2022-01-18 12:47:32 +0300

    Added countries search

--- lib/fcc4d/countries.rb
+++ lib/fcc4d/countries.rb
@@ -5,6 +5,16 @@ module FCC4D
         def ip ip
           client.get @content_type, api_path(['ip', ip])
         end
+
+        def search options
+          params = {
+            offset: options[:offset] || 0,
+            limit: options[:limit] || 0,
+            filter: options[:filter],
+            order: options[:order]
+          }
+          client.get @content_type, api_path(nil , params)
+        end
       end
     end
   end
