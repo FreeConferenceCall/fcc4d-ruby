85a76d9ac361dc9df8c15c1b47b6a27fd44ca9f8
Leonid Krintsyn <leonid.krinitsyn@freeconferencecall.com>
2022-06-28 14:37:38 +0300

    Passing options to did lookup request

--- lib/fcc4d/lookup.rb
+++ lib/fcc4d/lookup.rb
@@ -3,7 +3,8 @@ module FCC4D
     module V2
       class Lookup < Resource
         def dids did, options = {}
-          client.get @content_type, api_path(['dids', did])
+          params = options
+          client.get @content_type, api_path(['dids', did], params)
         end
 
         def ip_addresses ip
